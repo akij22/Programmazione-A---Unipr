11e6be4a91e15c3558c10188ee23007b0625849d
Aki <achi.roziiz@gmail.com>
2024-11-27 21:33:25 +0100

    ...,

--- Tasks/task7/cmake-build-debug/cmake_install.cmake
+++ Tasks/task7/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/achillerossi/Documents/UNIPR/C++/task7
+# Install script for directory: /Users/achillerossi/Documents/UNIPR/C++/Programmazione A/Tasks/task7
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/achillerossi/Documents/UNIPR/C++/task7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/achillerossi/Documents/UNIPR/C++/Programmazione A/Tasks/task7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
