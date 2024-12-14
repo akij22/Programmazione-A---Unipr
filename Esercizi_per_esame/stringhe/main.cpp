c43cf167fa28c7ff5fd0197811c509d6c875357c
Aki <achi.roziiz@gmail.com>
2024-12-14 13:34:31 +0100

    Completamento esercizio sulle stringhe

--- Esercizi_per_esame/stringhe/main.cpp
+++ Esercizi_per_esame/stringhe/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-using  namespace std;
 #include <cstring>
+using  namespace std;
+
 
 // Trova le sottostringhe di due parole
 bool findSubString (char* s1, char* s2) {
@@ -25,10 +26,40 @@ bool findSubString (char* s1, char* s2) {
 }
 
 
+/*
+ * Scrivere un programma in C++ che legge una stringa str e un carattere c
+ * e stampa a video una nuova stringa che corrisponde a str con tutte le occorrenze del carattere c rimosse.
+ */
+
+void deleteByChar (char* string, char c) {
+
+    bool found = false;
+
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == c) {
+            for (int j = i; string[j] != '\0'; j++) {
+                string[j] = string[j + 1];
+            }
+            found = true;
+
+            // Resetto l'indice per rianalizzare la stringa
+            i = 0;
+        }
+    }
+
+    if (found)
+        cout << string;
+    else
+        cout << "Carattere non presente nella stringa..." << endl;
+}
+
+
+
+
 int main() {
 
     // Lunghezza max della stringa = 100
-    char str1[101], str2[101];
+    char str1[101], str2[101], str3[51];
 
 
     cout << "Inserire la prima parola: ";
@@ -41,6 +72,19 @@ int main() {
         cout << "Esistono sottostringhe" << endl;
     else
         cout << "Non esistono sottostringhe" << endl;
+
+
+    do {
+        cout << "Inserire stringa: ";
+        cin.getline(str3, 50, '\n');
+    } while (cin.fail());
+
+    char toDelete;
+    cout << "Inserire carattere: ";
+    cin >> toDelete;
+
+    deleteByChar(str3, toDelete);
+
 
 
 
